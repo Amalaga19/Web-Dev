128fbd3c73567e21c29d12c91e0444b47a272cfb
Amalaga19 <andres.malaga1@gmail.com>
2023-05-03 12:57:20 -0400

    Works with local JSON now

--- public/script.js
+++ public/script.js
@@ -1,150 +1,48 @@
+document.getElementById('submitGuess').addEventListener('click', submitGuess);
+document.getElementById('countryGuess').addEventListener('keydown', handleKeyDown);
+document.getElementById('playAgain').addEventListener('click', () => location.reload());
 
-/*
-const hintEl = document.getElementById('hint');
-const guessEl = document.getElementById('guess');
-const submitBtn = document.getElementById('submit');
-const playAgainBtn = document.getElementById('playAgain');
-const guessedCountriesEl = document.getElementById('guessedCountries');
-const guessedCountEl = document.getElementById('guessedCount');
-const continentModal = document.getElementById('continentModal');
-const continentStats = document.getElementById('continentStats');
-const closeModal = document.querySelector('.close');
+let tries = 0;
+let currentCountry = null;
 
-const continents = {
-    'Africa': 0,
-    'Asia': 0,
-    'Europe': 0,
-    'North America': 0,
-    'Oceania': 0,
-    'South America': 0
-};
+fetchCountry();
 
-const getGuessedCountryCodes = () => {
-    const guessedCountryCodes = localStorage.getItem('guessedCountryCodes');
-    return guessedCountryCodes ? JSON.parse(guessedCountryCodes) : [];
-};
-
-const addGuessedCountryCode = (countryCode) => {
-    const guessedCountryCodes = getGuessedCountryCodes();
-    guessedCountryCodes.push(countryCode);
-    localStorage.setItem('guessedCountryCodes', JSON.stringify(guessedCountryCodes));
-};
-
-let countryCode;
-let hintIndex = 0;
-const hintsOrder = ['continentName', 'population', 'capital', 'countryCode'];
-
-const showNextHint = (country) => {
-    if (hintIndex >= hintsOrder.length) {
-        hintEl.innerHTML = 'No more hints available.';
-        return;
-    }
-    const hintKey = hintsOrder[hintIndex];
-    hintEl.innerHTML = `<strong>${hintKey}:</strong> ${country[hintKey]}`;
-    hintIndex++;
-};
-
-const getRandomCountry = async () => {
+async function fetchCountry() {
     try {
-        const guessedCountryCodes = getGuessedCountryCodes();
-        const response = await fetch('/api/countries/random', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ guessedCountryCodes })
-        });
-
-        if (response.status === 404) {
-            hintEl.innerHTML = 'All countries have been guessed! Clearing the list and starting over.';
-            localStorage.removeItem('guessedCountryCodes');
-            setTimeout(getRandomCountry, 2000);
-            return;
-        }
-
-        const country = await response.json();
-        countryCode = country.countryCode;
-        showNextHint(country);
-    } catch (err) {
-        console.error(err);
+        const response = await fetch('/api/countries/random');
+        currentCountry = await response.json();
+        document.getElementById('continentHint').innerText = `Continent: ${currentCountry.continentName}`;
+    } catch (error) {
+        console.error('Failed to fetch country:', error);
     }
-};
+}
 
-const checkGuess = async () => {
-    const guess = guessEl.value;
-    if (!guess) {
-        alert('Please enter a guess.');
-        return;
-    }
-
-    try {
-        const response = await fetch(`/api/countries/check/${countryCode}/${guess}`);
-        const result = await response.json();
-
-        if (result.isCorrect) {
-            addGuessedCountryCode(countryCode);
-            hintEl.innerHTML = `<strong>Correct!</strong> The country is ${guess}.`;
-            submitBtn.hidden = true;
-            playAgainBtn.hidden = false;
-            updateGuessedCountries();
-        } else {
-            alert('Incorrect. Try again.');
-            showNextHint(country);
-        }
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-const updateGuessedCountries = async () => {
-    const guessedCountryCodes = getGuessedCountryCodes();
-    guessedCountEl.textContent = guessedCountryCodes.length;
-
-    const response = await fetch('/api/countries');
-    const allCountries = await response.json();
-    const guessedCountries = allCountries.filter(country => guessedCountryCodes.includes(country.countryCode));
-
-    for (const continent in continents) {
-        continents[continent] = guessedCountries.filter(country => country.continentName === continent).length;
-    }
-
-    let statsHTML = '';
-    for (const continent in continents) {
-        const totalCount = allCountries.filter(country => country.continentName === continent).length;
-        const completedClass = continents[continent] === totalCount ? 'completed-continent' : '';
-        statsHTML += `<p class="${completedClass}">${continent}: ${continents[continent]}/${totalCount}</p>`;
-    }
-    continentStats.innerHTML = statsHTML;
-};
-
-submitBtn.addEventListener('click', checkGuess);
-guessEl.addEventListener('keydown', (event) => {
+function handleKeyDown(event) {
     if (event.key === 'Enter') {
-        checkGuess();
+        submitGuess();
     }
-});
+}
 
-playAgainBtn.addEventListener('click', () => {
-    hintIndex = 0;
-    getRandomCountry();
-    submitBtn.hidden = false;
-    playAgainBtn.hidden = true;
-});
+function numberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
-guessedCountriesEl.addEventListener('click', () => {
-    continentModal.style.display = 'block';
-});
-
-closeModal.addEventListener('click', () => {
-    continentModal.style.display = 'none';
-});
-
-window.addEventListener('click', (event) => {
-    if (event.target === continentModal) {
-        continentModal.style.display = 'none';
+function submitGuess() {
+    const guess = document.getElementById('countryGuess').value.toLowerCase();
+    if (guess === currentCountry.countryName.toLowerCase()) {
+        document.getElementById('message').innerText = 'Correct!';
+        document.getElementById('submitGuess').style.display = 'none';
+        document.getElementById('playAgain').style.display = 'block';
+    } else {
+        tries++;
+        if (tries === 1) {
+            document.getElementById('populationHint').innerText = `Population: ${numberWithCommas(currentCountry.population)}`;
+        } else if (tries === 2) {
+            document.getElementById('capitalHint').innerText = `Capital: ${currentCountry.capital}`;
+        } else {
+            document.getElementById('message').innerText = `Wrong, the correct answer was ${currentCountry.countryName}.`;
+            document.getElementById('submitGuess').style.display = 'none';
+            document.getElementById('playAgain').style.display = 'block';
+        }
     }
-});
-
-getRandomCountry();
-updateGuessedCountries();
-*/
+}
--- server.js
+++ server.js
@@ -2,21 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const config = require('./config');
 
-const PORT = config.PORT;
+const PORT = process.env.PORT || 3000;
 
-// ---- Connect to mongodb here ----
-// read in mongoose library
-const mongoose = require('mongoose');
-// read in the URI to our MongoDB Atlas 
-const MONGODB_URI = config.MONGODB_URI;
-// Use mongoose to connect to our MongoDB Atlas server
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+// ---- Load local JSON data here ----
+const countries = JSON.parse(fs.readFileSync(path.join(__dirname, 'countries.json'), 'utf8'));
 
-// --- connect to your collection ---
-
-const countries = require ('./models/allCountries');
+// --- Get a random country from the JSON data ---
+function getRandomCountry() {
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    return countries[randomIndex];
+}
 
 // Handle data in a nice way
 app.use(express.json());
@@ -27,143 +23,21 @@ app.use(express.static(publicURL));
 
 // Set your static html file
 app.get("/", (req, res) => {
-  res.sendFile( path.resolve(__dirname + "/views/index.html"))
+    res.sendFile(path.resolve(__dirname + "/views/index.html"));
 });
 
 // ---- ADD YOUR API ENDPOINTS HERE ----
-// ---- ADD YOUR API ENDPOINTS HERE ----
-// GET: "api/v1/countries"
-app.get("/api/v1/countries", async (req, res) => {
-    try{
-      //const data = await countries.find();
-      res.json({})
-    } catch(error){
-      console.error(error);
-      res.json(error);
+app.get('/api/countries/random', (req, res) => {
+    try {
+        const randomCountry = getRandomCountry();
+        res.json(randomCountry);
+    } catch (error) {
+        console.error('Failed to fetch a random country:', error);
+        res.status(500).send('Internal server error');
     }
-  });
-  
-  // POST: "api/v1/countries"
-  app.post("/api/v1/countries", async (req, res) => {
-    try{
-      res.json({})
-    } catch(error){
-      console.error(error);
-      res.json(error);
-    }
-  });
-  
-  // PUT: "api/v1/countries:id"
-  app.put("/api/v1/countries/:id", async (req, res) => {
-    try{
-      res.json({})
-    } catch(error){
-      console.error(error);
-      res.json(error);
-    }
-  });
-  
-  // DELETE: "api/v1/countries:id"
-  app.delete("/api/v1/countries/:id", async (req, res) => {
-    try{
-      res.json({})
-    } catch(error){
-      console.error(error);
-      res.json(error);
-    }
-  });
+});
 
 // Start listening
 app.listen(PORT, () => {
-  console.log(`see the magic: http://localhost:${PORT}`);
-})
-
-/*
-// server.js
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-
-dotenv.config();
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB Atlas');
-}).catch(err => {
-    console.error('Error connecting to MongoDB Atlas:', err);
+    console.log(`see the magic: http://localhost:${PORT}`);
 });
-
-// Country schema
-const countrySchema = new mongoose.Schema({
-    countryCode: String,
-    countryName: String,
-    currencyCode: String,
-    population: Number,
-    capital: String,
-    continentName: String
-});
-
-const Country = mongoose.model('Country', countrySchema);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-// Routes
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-// Get a random country
-app.post('/api/countries/random', async (req, res) => {
-    try {
-        const guessedCountryCodes = req.body.guessedCountryCodes || [];
-        const totalCountries = await Country.countDocuments({ countryCode: { $nin: guessedCountryCodes } });
-        if (totalCountries === 0) {
-            res.status(404).send('No more countries available');
-            return;
-        }
-        const randomIndex = Math.floor(Math.random() * totalCountries);
-        const randomCountry = await Country.findOne({ countryCode: { $nin: guessedCountryCodes } }).skip(randomIndex);
-        res.status(200).send(randomCountry);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-// Get all countries
-app.get('/api/countries', async (req, res) => {
-    try {
-        const countries = await Country.find();
-        res.status(200).send(countries);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-// Check if a country name matches a given country code
-app.get('/api/countries/check/:countryCode/:guess', async (req, res) => {
-    try {
-        const country = await Country.findOne({ countryCode: req.params.countryCode });
-        if (!country) {
-            res.status(404).send('Country not found');
-            return;
-        }
-        const isCorrect = country.countryName.toLowerCase() === req.params.guess.toLowerCase();
-        res.status(200).send({ isCorrect });
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-*/
